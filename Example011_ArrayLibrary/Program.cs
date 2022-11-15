fab28debd01c5e0e10fed0319f109e6a2dae9c2e
b_fang <shampuk@gmail.com>
2022-11-15 16:30:01 +0300

    add Example 011 Array Library

---
+++ Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,54 @@
+﻿//метод заполнения массива
+void FillArray(int[] collection) // void - метод, который не возвращает (return) значения
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0, 10);
+        // index = index+1;
+        index++;
+    }
+}
+
+// метод печати массива на экран
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while(index < count)
+    {
+        if (collection[index] ==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+    
+
+    
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
